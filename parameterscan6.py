54ebddd78a8822cc9654fd3232352f082a55aade
Liam1337V <167616041+Liam1337V@users.noreply.github.com>
2025-05-21 15:13:15 +0200

    Update parameterscan6.py

--- parameterscan6.py
+++ parameterscan6.py
@@ -6,78 +6,102 @@ import os
 
 # Parameters
 # TODO adapt to what you need (folder path executable input filename)
-executable = r"/Users/Matte/Desktop/EPFL/BA4/Physnum/Physnum_Exo4/exe" #r"/Users/a-x-3/Desktop/Ex3_2024_student/exe"  # Name of the executable (NB: .exe extension is required on Windows)
-repertoire = r"/Users/Matte/Desktop/EPFL/BA4/Physnum/Physnum_Exo4"#r"/Users/a-x-3/Desktop/Ex3_2024_student"
+executable = r"/Users/a-x-3/Desktop/Exercice6_2025_student/exe"  # Name of the executable (NB: .exe extension is required on Windows)
+repertoire = r"/Users/a-x-3/Desktop/Exercice6_2025_student"
 os.chdir(repertoire)
 
 input_filename = 'configuration.in.example'  # Name of the input file
 
-#----------------------------------------- Valeurs du Configfile --------------------------------- # 
+# ------------------------------------- Simulations ----------------------------------- #
 
-Values = np.genfromtxt("/Users/Matte/Desktop/EPFL/BA4/Physnum/Physnum_Exo4/configuration.in.example")
+#nsteps = np.array([2,3,4,5,6,7,8,9])*200 # valeurs utilisées pour la convergence
+#nx = np.array([2,3,4,5,6,7,8,9])*20 # valeurs utilisées pour la convergence
 
-#R = Values[0,-1]
-#r1 = Values[1,-1]
-#epsilon_a = Values[2,-1]
-#epsilon_b= Values[3,-1]
-#uniform_rho_case = Values[4,-1]
-#VR = Values[5,-1]
-#rho0  =  Values[6,-1]
-N1    = Values[9,-1]
-#N2  = Values[8,-1]
-#maxit  = Values[9,-1]
-#output = Values[10,-1]
-#sampling = Values[11,-1]
-#verbose = Values[12,-1]
-# ---------------------------------------------------------------
+#nsteps = np.array([50e3]) # valeurs utilisées pour le tsunami
+#nx = np.array([10000]) # valeurs utilisées pour le tsunami 
+
+Nsteps = np.array([800]) 
+Nintervals = np.array([512])
+
+paramstr = 'Nsteps'  # Parameter name to scan
+param = Nsteps  # Parameter values to scan
+
+paramstr2 = 'Nintervals'  # Parameter name to scan
+param2 = Nintervals  # Parameter values to scan
+
+nsimul = len(Nsteps)
+
+# Simulations
+outputs = []  # List to store output file names
+convergence_list = []
+for i in range(nsimul):
+    output_file = f"{paramstr}={param[i]}_{paramstr2}={param2[i]}.out"
+    outputs.append(output_file)
+    cmd = f"{repertoire}{executable} {input_filename} {paramstr}={param[i]:.15g} output={output_file}"
+    cmd = f"{executable} {input_filename} {paramstr}={param[i]:.15g} {paramstr2}={param2[i]:.15g} output={output_file}"
+    print(cmd)
+    subprocess.run(cmd, shell=True)
+    print('Done.')
+
 lw = 1.5
 fs = 16
-N1_list = [16, 32, 64, 128, 256]  # exemple
-phi_center_list = []
-config_path = "/Users/Matte/Desktop/EPFL/BA4/Physnum/Physnum_Exo4/configuration.in.example"
-phi_output_path = "/Users/Matte/Desktop/EPFL/BA4/Physnum/Physnum_Exo4/phioutput.out"
-# ------------------------------------------- Simulations ---------------------------------------------
 
-data = np.loadtxt("/Users/Matte/Desktop/EPFL/BA4/Physnum/Physnum_Exo4/phioutput.out")
-data2 = np.loadtxt("/Users/Matte/Desktop/EPFL/BA4/Physnum/Physnum_Exo4/Eoutput.out") # Load the output file of the i-th simulation
-r = data[:, 0]
-phi = data[:,1] # nombre de pas de temps total pour la simulation
-rE= data2[:,0]
-E = data2[:,1]
-print(phi,N1)
-def conv(): #phi(r=0) en fonction du nombre de points de maillage N1(=N2)
-    for N1 in N1_list:
-        # Charger et modifier le fichier de configuration
-        config_data = np.genfromtxt(config_path)
-        config_data[10, -1] = N1  # Modifier la ligne correspondant à N1
-        np.savetxt(config_path, config_data, fmt="%.8e")  # Sauvegarder
+psi2 = np.loadtxt(outputs[-1]+"_psi2.out")
+obs = np.loadtxt(outputs[-1]+ "_obs.out")
+pot = np.loadtxt(outputs[-1]+ "_pot.out")
 
-        # Lancer l'exécutable
-        subprocess.run([executable])
+t = obs[0,:]
+x = pot[0,:]
 
-        # Lire la sortie (phi au centre)
-        data = np.loadtxt(phi_output_path)
-        phi = data[:, 1]  # colonne du potentiel
-        phi_center = phi[0]  # ou un autre critère : moyenne, etc.
-        phi_center_list.append(phi_center)
-        print(phi)
 
-    # Tracer la convergence
+def xmoy_t (obs) :
+
+    t = obs[:,0]
+    xmoy = obs[:,4]
+
+    #for i in xmoy :
+        #print(i)
+    #for i in t :
+        #print(i)
+
+    #plt.scatter(1,1)
     plt.figure()
-    plt.plot(N1_list, phi_center_list, 'ko-', linewidth=lw)
-    plt.xlabel("Nombre de points N1", fontsize=fs)
-    plt.ylabel(r"$\phi(r=0)$ (V)", fontsize=fs)
-    plt.title("Convergence de $\phi(r=0)$ en fonction de N1", fontsize=fs)
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
+    plt.plot(t,xmoy)
+    plt.xlabel("Temps [s]", fontsize = fs)
+    plt.ylabel("$x_{moy}(t)$", fontsize = fs)
 
-def pot():
-    plt.plot(r,phi)
-    #plt.plot(rE,E)
-    plt.xlabel("r,rE", fontsize=fs)
-    plt.ylabel("phi, E", fontsize=fs)
-    plt.grid()
-pot()
-conv() #marche pas 
+def pmoy_t (obs) :
+
+    t = obs[:,0]
+    pmoy = obs[:,6]
+
+    plt.figure()
+    plt.plot(t,pmoy)
+    plt.xlabel("Temps [s]", fontsize = fs)
+    plt.ylabel("$p_{moy}(t)$", fontsize = fs)
+
+def Vplot(pot) :
+
+    x = pot[:,0]
+    V = pot[:,1]
+
+    plt.figure()
+    plt.plot(x,V)
+    plt.xlabel("x []", fontsize = fs)
+    plt.ylabel("V", fontsize = fs)
+
+def ColorPlot (obs,psi,pot) : 
+
+    t = obs[:,0]
+    x = pot[:,0]
+    abs_ = psi[:,0]
+
+    plt.pcolor(x,t,abs_)
+    
+
+    print(psi.shape)
+
+xmoy_t(obs)
+pmoy_t(obs)
 plt.show()
+
